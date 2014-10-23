764759b4096ce4088e822e3bd22988a12145b477
Robert Harm <robert@harm.co.at>
2014-10-23 20:21:08 +0200

    update pro upgrader

--- inc/changelog.php
+++ inc/changelog.php
@@ -50,7 +50,7 @@ echo '<p style="margin:0.5em 0 0 0;"><strong>' . sprintf($text_a, '3.9.4') . '</
 
 </td></tr>
 <tr><td>' . $changed . '</td><td>
-
+removed link to ioncube encoded pro plugin package to increase compatibility with PHP5.5+
 </td></tr>
 <tr><td>' . $fixed . '</td><td>
 WMS legend link on frontend and fullscreen maps was broken (thx Graham!)
